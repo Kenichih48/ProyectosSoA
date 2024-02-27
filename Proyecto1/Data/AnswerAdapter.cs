37ea163408bff567251b89b3d127cfe3d218c900
Quigonar <67380073+Quigonar@users.noreply.github.com>
2024-02-27 11:21:44 -0600

    Create AnswerAdapter.cs

---
+++ Proyecto1/Data/AnswerAdapter.cs
@@ -0,0 +1,65 @@
+namespace MyService.Data
+{
+    public class AnswerAdapter
+    {
+        private APIDbContext apiDBContext;
+        private Random random = new Random();
+        public AnswerAdapterData(APIDbContext apiDBContext)
+        {
+            this.apiDBContext = apiDBContext;
+        }
+    
+        public async Task<string> RetrieveDataFromDatabase()
+        {
+            // CHANGE jsonData TO WHATEVER WE USE TO GET THE DATA FROM THE DATABASE
+            // jsonData = await AnswerAdapterData.GetData();
+
+            return jsonData;
+        }
+        public async Task<string> RetrieveDataFromOpenAIAPI()
+        {
+            // CHANGE jsonData TO WHATEVER WE USE TO GET THE DATA FROM THE OPENAI API
+            //var response = await httpClient.GetAsync(openAIApiEndpoint);
+            //var jsonData = await response.Content.ReadAsStringAsync();
+
+            return jsonData;
+        }
+        public async Task<string> RetrieveDataFromExternalEndPoint()
+        {
+            // CHANGE jsonData TO WHATEVER WE USE TO GET THE DATA FROM THE EXTERNALENDPOINTAPI
+            // var response = await httpClient.GetAsync(externalEndPointApiEndpoint);
+            // var jsonData = await response.Content.ReadAsStringAsync();
+
+            return jsonData;
+        }
+        public string AdaptAnswerToDesiredFormat(string source)
+        {
+            string jsonData = "";
+
+            switch (source.ToLower())
+            {
+                case "database":
+                    // Retrieve data from the database
+                    jsonData = RetrieveDataFromDatabase().Result;
+                    break;
+                case "openai":
+                    // Retrieve data from the OpenAI API
+                    jsonData = RetrieveDataFromOpenAIAPI().Result;
+                    break;
+                case "externalep":
+                    // Retrieve data from the ExternalEndPoint API
+                    jsonData = RetrieveDataFromExternalEndPoint().Result;
+                    break;
+                default:
+                    throw new ArgumentException("Invalid data source provided.");
+            }
+
+            return TransformJsonToDesiredFormat(jsonData);
+        }
+        private string TransformJsonToDesiredFormat(string jsonData)
+        {
+            // LOGIC TO TRANSFORM ANY GIVEN DATA TO OUR NEEDED ANSWER FORMAT
+            return jsonData;
+        }
+    }
+}
