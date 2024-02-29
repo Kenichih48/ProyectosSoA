36369149ea1dcdba99ba9ba4e810ec01751a7ab5
Jose Retana <joseantonioretanacorrales@gmail.com>
2024-02-28 23:22:58 -0600

    upsie

--- MyServiceAPI/MyServiceAPI/Program.cs
+++ MyServiceAPI/MyServiceAPI/Program.cs
@@ -1,7 +1,9 @@
+using MyServiceAPI.Configurations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.Configure<OpenAiConfiguration>(builder.Configuration.GetSection("OpenAI"));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
