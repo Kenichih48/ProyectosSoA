7b4912082fc28cfe60f7f3197c1da5314b57c3c2
Kenichih48 <Kenichih48@yahoo.com>
2024-02-28 20:43:26 -0600

    Boilerplate del proyecto

---
+++ MyServiceAPI/MyServiceAPI/Controllers/UserRequestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyServiceAPI.Controllers
+{
+    [ApiController]
+    [Route("MyService")]
+    public class UserRequestController
+    {
+        GetFullMeal
+
+        GetDessert
+
+        GetLunch
+
+        GetDrink
+    }
+}
---
+++ MyServiceAPI/MyServiceAPI/UserRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace MyServiceAPI
+{
+    public class UserRequest
+    {
+        public int Id { get; set; }
+        public string? comida { get; set; }
+        public string? tipo { get; set; }
+    }
+}
