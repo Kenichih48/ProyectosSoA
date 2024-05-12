e1bc1a7de36acc6b74516fff7396d37219d028a0
Kenichih48 <Kenichih48@yahoo.com>
2024-05-11 23:38:33 -0600

    Database changed from SSMS to MySQL

--- MyRESTaurantAPI-python/Database/MyRestaurantCreation.sql
+++ MyRESTaurantAPI-python/Database/MyRestaurantCreation.sql
@@ -7,47 +7,44 @@ USE MyRestaurantDataBase;
 
 -- Crear la tabla Usuario
 CREATE TABLE Usuario (
-    id INT IDENTITY(1,1) PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     contrasena VARCHAR(255) NOT NULL,
     correo VARCHAR(255) NOT NULL,
     nombre VARCHAR(100) NOT NULL,
     apellido VARCHAR(100) NOT NULL,
     direccion VARCHAR(255) NOT NULL,
-	nivel_acceso BIT NOT NULL,
+    nivel_acceso TINYINT(1) NOT NULL
 );
 
 -- Crear la tabla Reservaciones
 CREATE TABLE Reservaciones (
-    id INT IDENTITY(1,1) PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     hora TIME NOT NULL,
     fecha DATE NOT NULL,
-	estado BIT NOT NULL,
-	people_quant INT,
-	id_usuario INT,
-	FOREIGN KEY (id_usuario) REFERENCES Usuario(id)
+    estado TINYINT(1) NOT NULL,
+    people_quant INT,
+    id_usuario INT,
+    FOREIGN KEY (id_usuario) REFERENCES Usuario(id)
 );
 
-
--- Crear la tabla TIPO_Platillos
-CREATE TABLE Tipo_Platillos(
-
-	id INT IDENTITY(1,1) PRIMARY KEY,
-	tipo VARCHAR(50)
-
+-- Crear la tabla Tipo_Platillos
+CREATE TABLE Tipo_Platillos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    tipo VARCHAR(50)
 );
 
 -- Crear la tabla Platillos
 CREATE TABLE Platillos (
-    id INT IDENTITY(1,1) PRIMARY KEY,
-	tipo_id INT,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    tipo_id INT,
     nombre VARCHAR(30) NOT NULL,
-    descripcion VARCHAR(255)
-	FOREIGN KEY (tipo_id) REFERENCES Tipo_Platillos(id)
+    descripcion VARCHAR(255),
+    FOREIGN KEY (tipo_id) REFERENCES Tipo_Platillos(id)
 );
 
 -- Crear la tabla Recomendaciones
 CREATE TABLE Recomendaciones (
-    id INT IDENTITY(1,1) PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     id_platillo INT,
     set_rec INT,
     FOREIGN KEY (id_platillo) REFERENCES Platillos(id)
